d08c9156fa8013235aa474e57aca66c566e0d59b
mebelh <memetbelualov@bk.ru>
2020-06-25 13:17:41 +0300

    start working on auth

--- routs/todo.js
+++ routs/todo.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    if (!req.session.isAuthentificated) {
+    if (req.session.isAuthentificated) {
         res.render("../views/todo.hbs", {
             title: "Менеджер клиентов",
             isTodo: true,
@@ -20,5 +20,7 @@ router.get("/login", (req, res) => {
         layout: "todo",
     });
 });
+
+router.post("/login", (req, res) => {});
 
 module.exports = router;
